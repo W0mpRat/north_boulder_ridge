aeee378881b297a4217f4b1a291b8fc1fa66bc5d
Chase Anderson <anderson.chasew@gmail.com>
2019-08-16 18:10:18 -0600

    progress

--- index.js
+++ index.js
@@ -1,31 +1,51 @@
 const schedule = require('node-schedule')
 const axios = require('axios')
-const windyStationId = 0
 const apiKey = process.env.WINDY_API_KEY
+const wunderGroundApiKey = '6532d6454b8aa370768e63d6ba5a832e'
+const stations = [
+  {
+    windyId: 0,
+    wundergroundName: 'KCOBOULD45'
+  },
+  {
+    windyId: 1,
+    wundergroundName: 'KCOBOULD442'
+  }
+]
 
 const j = schedule.scheduleJob('10 */5 * * * *', async function () {
   console.log(new Date())
-  const observations = await getWeatherData()
-  await sendWeatherData(observations)
+  // for (const station of stations) {
+  const observations = await getWeatherData(stations[0].wundergroundName)
+  await sendWeatherData(stations[0], observations)
+  // }
 })
 
-async function getWeatherData () {
+const j2 = schedule.scheduleJob('15 */5 * * * *', async function () {
+  console.log(new Date())
+  // for (const station of stations) {
+  const observations = await getWeatherData(stations[1].wundergroundName)
+  await sendWeatherData(stations[1], observations)
+  // }
+})
+
+async function getWeatherData (wundergroundName) {
   try {
-    const resultImperial = await axios.get('https://api.weather.com/v2/pws/observations/current?apiKey=6532d6454b8aa370768e63d6ba5a832e&stationId=KCOBOULD45&numericPrecision=decimal&format=json&units=e')
-    const resultMetric = await axios.get('https://api.weather.com/v2/pws/observations/current?apiKey=6532d6454b8aa370768e63d6ba5a832e&stationId=KCOBOULD45&numericPrecision=decimal&format=json&units=m')
+    const resultImperial = await axios.get(`https://api.weather.com/v2/pws/observations/current?apiKey=${wunderGroundApiKey}&stationId=${wundergroundName}&numericPrecision=decimal&format=json&units=e`)
+    const resultMetric = await axios.get(`https://api.weather.com/v2/pws/observations/current?apiKey=${wunderGroundApiKey}&stationId=${wundergroundName}&numericPrecision=decimal&format=json&units=m`)
     return [resultImperial.data.observations[0], resultMetric.data.observations[0]]
   } catch (error) {
     console.error(`Get Data Failed: ${error}`)
   }
 }
 
-async function sendWeatherData (observations) {
+async function sendWeatherData (station, observations) {
   try {
     const imperialObservation = observations[0]
     const metricObservation = observations[1]
     const params = {
       observations: [{
-        station: windyStationId,
+        station: station.windyId,
         dateutc: imperialObservation.obsTimeUtc.replace('Z', ''),
         tempf: imperialObservation.imperial.temp,
         windspeedmph: imperialObservation.imperial.windSpeed,
@@ -37,7 +57,7 @@ async function sendWeatherData (observations) {
       }]
     }
     const result = await axios.post(`https://stations.windy.com/pws/update/${apiKey}`, params)
-    console.log(`Observation Posted ${new Date()}`)
+    console.log(`${station.wundergroundName} Observation Posted ${new Date()}`)
     console.log(observations)
     console.log(params.observations[0])
 
